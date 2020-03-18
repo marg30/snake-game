94d6693d83999260ea1532648a9ba138aad5de1f
marg30 <marianagaldomartinez@gmail.com>
2020-03-18 14:07:42 -0300

    first commit

---
+++ web/singlephase/public/js/singlephase.js
@@ -0,0 +1,11 @@
+import MainScene from "./MainScene.js";
+
+const config = {
+    width: 480,
+    height: 480,
+    type: Phaser.AUTO,
+    parent: 'phaser-game',
+    scene: [MainScene]
+};
+
+new Phaser.Game(config);
